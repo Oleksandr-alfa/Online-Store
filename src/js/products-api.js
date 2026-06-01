5d12e79706835b8aae236719d9d67b71861e6527
Oleksandr-alfa <a_kos_76_@ukr.net>
2026-06-01 09:15:43 +0300

    update categories

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,8 +1,14 @@
 import axios from "axios"
 import { BASE_URL, END_POINTS } from "./constants"
 
+axios.defaults.baseURL = BASE_URL;
+
 export const fetchAllCategories = async () => {
-    const response = await axios.get(`${BASE_URL}${END_POINTS.allCategory}`);
+    const response = await axios.get(END_POINTS.allCategory);
     console.log(response);
+    return response.data;
+}
+export const fetchAllProducts = async (currentPage) => {
+    const response = await axios.get(END_POINTS.allProducts,{params: {LIMIT, scip: (currentPage - 1) * LIMIT} });
     return response.data;
 }
